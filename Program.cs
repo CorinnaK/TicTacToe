4239c1332caaf7c870ceb194033f863b369d6f92
CorinnaK <corinna.kelly@outlook.com>
2020-06-26 09:09:37 -0600

    Created one square with the number one in it

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Tic Tac Toe Game");
+            Console.WriteLine(" _ _ _");
+            Console.WriteLine("|     |");
+            Console.WriteLine("|  1  |");
+            Console.WriteLine("|_ _ _|");
         }
     }
 }
