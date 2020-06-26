709a781d581fcb035458c82c305932b29b0a093a
CorinnaK <corinna.kelly@outlook.com>
2020-06-26 14:14:52 -0600

    Added draw three squares in a row

--- Program.cs
+++ Program.cs
@@ -7,10 +7,19 @@ namespace Tic_Tac_Toe
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Tic Tac Toe Game");
-            Console.WriteLine(" _ _ _");
-            Console.WriteLine("|     |");
-            Console.WriteLine("|  1  |");
-            Console.WriteLine("|_ _ _|");
+            CreateSquare();
+        
+        }
+        static void CreateSquare()
+        {
+            string a = " _ _ _";
+            string b = "|     |";
+            string c = "|  1  |";
+            string d = "|_ _ _|";
+            Console.WriteLine(a + a + a);
+            Console.WriteLine(b + b + b);
+            Console.WriteLine(c + c + c);
+            Console.WriteLine(d + d + d);
         }
     }
 }
