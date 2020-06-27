43c29ae4e6b1840af05b2dec482d6ffcfc4aabd0
CorinnaK <corinna.kelly@outlook.com>
2020-06-26 20:39:05 -0600

    Defualt Board and Current Board state

--- Program.cs
+++ Program.cs
@@ -7,19 +7,58 @@ namespace Tic_Tac_Toe
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Tic Tac Toe Game");
-            CreateSquare();
+            DefaultBoard();
+            CurrentBoardState();
         
         }
-        static void CreateSquare()
+        
+        static void DefaultBoard()
+        // Method to create a tic tac toe board that shows player what number represents which square
+        // Spend way to much time on this section and over thought it
+        // Plus too pretty to start over
         {
-            string a = " _ _ _";
-            string b = "|     |";
-            string c = "|  1  |";
-            string d = "|_ _ _|";
-            Console.WriteLine(a + a + a);
-            Console.WriteLine(b + b + b);
-            Console.WriteLine(c + c + c);
-            Console.WriteLine(d + d + d);
+            int squareNum = 1;
+            // string topLine = " _ _ _ ";
+            // string spacer = "|     |";
+            // string spacerNumber = "|  {0}  |";
+            // string lastLine = "|_ _ _|";
+             
+            for (int i = 0; i < 3; i++)
+            // Loop to create three rows
+            {
+                for (int j = 0; j < 3; j++)
+                // Loop to create three columns in each row
+                {
+                    Console.Write(" _ _ _ ");
+                    // Use Write so that it doesn't use line terminator
+                }
+                // Need a new line after 
+                Console.WriteLine("");
+                for (int j = 0; j < 3; j++)    
+                {
+                    Console.Write("|     |");
+                }    
+                Console.WriteLine("");
+                for (int j = 0; j < 3; j++)    
+                {
+                    Console.Write("|  {0}  |", squareNum);
+                    squareNum++;
+                }    
+                Console.WriteLine("");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("|_ _ _|");
+                }
+                Console.WriteLine("");
+            }
         }
+        static void CurrentBoardState()
+        {
+            Console.WriteLine("Hello, World");
+        }
+
+    
     }
 }
+
+
