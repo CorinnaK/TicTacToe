a79bd4759aaff2781de26253bbed68ee5d1dbd2a
CorinnaK <corinna.kelly@outlook.com>
2020-06-26 21:23:18 -0600

    Changed Default board to board with if statement for default or current state and completely re-wrote, added test arrays

--- Program.cs
+++ Program.cs
@@ -6,52 +6,47 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            string [,] board = {{"0","X", "2"},{"3","4","5"},{"6","7", "8"}}; 
             Console.WriteLine("Welcome to the Tic Tac Toe Game");
-            DefaultBoard();
+            DefaultBoard("default", board);
             CurrentBoardState();
         
         }
         
-        static void DefaultBoard()
+        static void DefaultBoard(string state, string[,] currentArray)
         // Method to create a tic tac toe board that shows player what number represents which square
         // Spend way to much time on this section and over thought it
-        // Plus too pretty to start over
-        {
-            int squareNum = 1;
-            // string topLine = " _ _ _ ";
-            // string spacer = "|     |";
-            // string spacerNumber = "|  {0}  |";
-            // string lastLine = "|_ _ _|";
-             
-            for (int i = 0; i < 3; i++)
-            // Loop to create three rows
+        {   
+           
+            if (state == "default")
             {
-                for (int j = 0; j < 3; j++)
-                // Loop to create three columns in each row
-                {
-                    Console.Write(" _ _ _ ");
-                    // Use Write so that it doesn't use line terminator
-                }
-                // Need a new line after 
-                Console.WriteLine("");
-                for (int j = 0; j < 3; j++)    
-                {
-                    Console.Write("|     |");
-                }    
-                Console.WriteLine("");
-                for (int j = 0; j < 3; j++)    
-                {
-                    Console.Write("|  {0}  |", squareNum);
-                    squareNum++;
-                }    
-                Console.WriteLine("");
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write("|_ _ _|");
-                }
-                Console.WriteLine("");
+                Console.WriteLine("Here is the default board marking the spaces with the number representation");
+                string [,] squareArr = {{"0","1", "2"},{"3","4","5"},{"6","7", "8"}};                      
+                Console.WriteLine("      |       |        ");
+                Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[0,0], squareArr[0,1], squareArr[0,2]);
+                Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
+                Console.WriteLine("      |       |        ");
+                Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[1,0], squareArr[1,1], squareArr[1,2]);
+                Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
+                Console.WriteLine("      |       |        ");
+                Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[2,0], squareArr[2,1], squareArr[2,2]);
+                Console.WriteLine("      |       |        ");
             }
-        }
+            else
+            {
+                string [,] squareArr = currentArray;
+                Console.WriteLine("      |       |        ");
+                Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[0,0], squareArr[0,1], squareArr[0,2]);
+                Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
+                Console.WriteLine("      |       |        ");
+                Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[1,0], squareArr[1,1], squareArr[1,2]);
+                Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
+                Console.WriteLine("      |       |        ");
+                Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[2,0], squareArr[2,1], squareArr[2,2]);
+                Console.WriteLine("      |       |        ");                         
+            }
+        }     
+
         static void CurrentBoardState()
         {
             Console.WriteLine("Hello, World");
