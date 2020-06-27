d18f03d967fb2c61182dce03ff6991ab38594052
CorinnaK <corinna.kelly@outlook.com>
2020-06-26 21:28:43 -0600

    Add minor cosmetic fixes

--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            string [,] board = {{"0","X", "2"},{"3","4","5"},{"6","7", "8"}}; 
+            string [,] board = {{" ","X", " "},{" "," "," "},{" "," ", " "}}; 
             Console.WriteLine("Welcome to the Tic Tac Toe Game");
             DefaultBoard("default", board);
+            Console.WriteLine("This is the current state with 1 marked as x");
+            DefaultBoard("current", board);
             CurrentBoardState();
         
         }
@@ -22,7 +24,7 @@ namespace Tic_Tac_Toe
             {
                 Console.WriteLine("Here is the default board marking the spaces with the number representation");
                 string [,] squareArr = {{"0","1", "2"},{"3","4","5"},{"6","7", "8"}};                      
-                Console.WriteLine("      |       |        ");
+                Console.WriteLine("\n      |       |        ");
                 Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[0,0], squareArr[0,1], squareArr[0,2]);
                 Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
                 Console.WriteLine("      |       |        ");
@@ -30,12 +32,13 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
                 Console.WriteLine("      |       |        ");
                 Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[2,0], squareArr[2,1], squareArr[2,2]);
-                Console.WriteLine("      |       |        ");
+                Console.WriteLine("      |       |        \n");
+                
             }
             else
             {
                 string [,] squareArr = currentArray;
-                Console.WriteLine("      |       |        ");
+                Console.WriteLine("\n      |       |        ");
                 Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[0,0], squareArr[0,1], squareArr[0,2]);
                 Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
                 Console.WriteLine("      |       |        ");
@@ -43,7 +46,7 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
                 Console.WriteLine("      |       |        ");
                 Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[2,0], squareArr[2,1], squareArr[2,2]);
-                Console.WriteLine("      |       |        ");                         
+                Console.WriteLine("      |       |        \n");                         
             }
         }     
 
