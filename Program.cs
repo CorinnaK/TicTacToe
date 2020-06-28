7bc5f0c55e8b1a6ebc4caba3f1b7acc5f0dff13a
CorinnaK <corinna.kelly@outlook.com>
2020-06-28 15:32:25 -0600

    ReFactored PlayAgain, Handled draw situations, Fixed Bug wrong win for 2,4,6

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Tic_Tac_Toe
 {
@@ -17,6 +18,7 @@ namespace Tic_Tac_Toe
             string instructions1 = "The game is played on a grid that's 3 squares by 3 squares. The first player is X and the second is O. \nPlayers take turns selecting a square to put their marks in.The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner. \nWhen all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends with the CAT winning (a draw or tie). \n";
             string instructions2 = "Please note that all the squares are numbered to their corresponding location. \nTo make your move please type the number of an empty square you would like to put your mark in when prompted on your turn. \nThe board will refresh after each turn. \n \nCurrent Board in Play";
             string gameState = "playOn";
+            // Create a 2D array board with a space to maintain the visual of the drawn board 
             string[,] currentBoard = {{ " ", " ", " " },{ " ", " ", " " },{ " ", " ", " " }};
             while (gameState != "DONE")
             {
@@ -28,14 +30,17 @@ namespace Tic_Tac_Toe
                 PrintGameBoard(instructionBoard);
                 Console.WriteLine(instructions2);
                 PrintGameBoard(currentBoard);
-                //Console.WriteLine(gameState);
                 Console.WriteLine("Player {0}, it is your turn. Please select an empty square: ", currentPlayer );
                 string userSelection = Console.ReadLine();
                 currentBoard = PlayGame(userSelection, currentBoard, currentPlayer);
-                Console.WriteLine("Successful selection made"); // Pause here please for a few seconds                
+                Console.WriteLine("Successful selection made");
+                // @ Source https://www.tutorialspoint.com/Chash-Program-to-Pause-a-Thread#:~:text=C%23%20Program%20to%20Pause%20a%20Thread.%20To%20pause,set%20the%20sleep%20method%20to%20pause%20the%20thread.
+                // Needed for user experience to pause so the message would display long enough to read parameter is in milliseconds
+                Thread.Sleep(500); // Pause here please for a few seconds                
                 gameState = CheckForWinningCombination(currentBoard, currentPlayer);
-                //Console.WriteLine(gameState);
                 currentPlayer = ChangePlayer(currentPlayer);
+                Console.WriteLine("Test");
+                
                 Console.Clear();
             }            
         }
@@ -75,26 +80,25 @@ namespace Tic_Tac_Toe
             currentArray[0,1]==currentArray[1,1] && currentArray[2,1] == currentArray[0,1] && currentArray[0,1] != " " ||
             currentArray[0,2]==currentArray[1,2] && currentArray[2,2] == currentArray[0,2] && currentArray[0,2] != " " ||
             currentArray[0,0]==currentArray[1,1] && currentArray[2,2] == currentArray[0,0] && currentArray[0,0] != " " ||
-            currentArray[0,2]==currentArray[1,1] && currentArray[2,0] == currentArray[2,2] && currentArray[0,2] != " ")
+            currentArray[0,2]==currentArray[1,1] && currentArray[2,0] == currentArray[0,2] && currentArray[0,2] != " ")
             {
-                Console.WriteLine("Congratulations Player {0}, you are the winner!!!!\n \n Would you like to play again? (Y/N) ...", currentPlayer);
-                string playAgain = (Console.ReadLine()).ToUpper();
-                if (playAgain == "Y")
-                {
-                    //currentArray = new string[,]{{" "," ", " "},{" "," "," "},{" "," ", " "}};
-                    return "RESET";
-                }
-                else if (playAgain == "N")
-                {
-                    return "DONE";
-                }
-                else
-                {
-                    Console.WriteLine("That was an invalid selection. Exiting now.");
-                    return "DONE";
-                }
+                PrintGameBoard(currentArray);
+                Console.WriteLine("Congratulations Player {0}, you are the winner!!!!",currentPlayer);
+                return PlayAgain();      
             }
-            return "playOn";
+            // Checking each square in the array to see if it the board is full
+            for (int i = 0; i < currentArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < currentArray.GetLength(1); j++)
+                    {
+                        if (currentArray[i,j] == " ")
+                        {
+                            return "PlayOn";
+                        }
+                    }
+                }
+            Console.WriteLine(" It is a Draw! The CAT has won --- Meow!");
+            return PlayAgain();
         }
         static string[,] PlayGame (string userSelection, string[,] currentArray, string currentPlayer)
         // Method to check the users selection for validity - valid unoccupied square and square number
@@ -209,8 +213,27 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("That is an invalid square selection please try again!\nPlease try again by entering a valid square number:...");
                 userSelection = Console.ReadLine();           
             }
-        } 
-    
+        }
+        static string PlayAgain()
+        {
+            Console.Write("Would you like to play again? (Y/N) ..."); 
+            string playAgain = (Console.ReadLine()).ToUpper();
+            if (playAgain == "Y" || playAgain == "YES")
+            {
+                return "RESET";
+            }
+                else if (playAgain == "N")
+            {
+                return "DONE";
+            }
+            else
+            {
+                Console.WriteLine("That was an invalid selection. Exiting now.");
+                Thread.Sleep(500);
+                return "DONE";
+            }
+        }
+
     }
 }
 
