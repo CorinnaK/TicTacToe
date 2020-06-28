4b0dc76af17ff0f73ec2021a11db4ea6d22fe159
CorinnaK <corinna.kelly@outlook.com>
2020-06-28 01:49:37 -0600

    Bugged with Play Again reset

--- Program.cs
+++ Program.cs
@@ -7,48 +7,37 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            
+            Console.Clear();
             string [,] instructionBoard = {{"0","1","2"},{"3","4","5"},{"6","7","8"}};
-            string [,] currentBoard = {{" "," ", " "},{" ","X"," "},{" "," ", " "}};
-
+            string [,] defaultBoard = {{" "," ", " "},{" "," "," "},{" "," ", " "}};
             string currentPlayer = "X";
             // Source https://www.exploratorium.edu/brain_explorer/tictactoe.html
             // Copied the rules for Tic Tac Toe for readability and user understanding
             string instructions1 = "The game is played on a grid that's 3 squares by 3 squares. The first player is X and the second is O. \nPlayers take turns selecting a square to put their marks in.The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner. \nWhen all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends with the CAT winning (a draw or tie). \n";
             string instructions2 = "Please note that all the squares are numbered to their corresponding location. \nTo make your move please type the number of an empty square you would like to put your mark in when prompted on your turn. \nThe board will refresh after each turn. \n \nCurrent Board in Play";
-            string winner = "playOn";
-            Console.WriteLine(instructions1);
-            PrintGameBoard(instructionBoard);
-            Console.WriteLine(instructions2);
-            PrintGameBoard(currentBoard);
-            Console.WriteLine("Player {0}, it is your turn. Please select an empty square: ", currentPlayer );
-            string userSelection = Console.ReadLine();
-            int square = CheckValidPlay(userSelection, currentBoard);
-            Console.WriteLine("Successful selection made");
-
-
-            // repeat while win codition not met -- PlayOn
-            // Print instruction board !
-            // Print Which Players turn
-            // Print Current CurrentBoard
-            // Print Prompt to User
-            // Retrieve User play
-            // Update board
-            // Check for winner
-            // Switch player
-            // Clear terminal
-        
-            
-            // Console.WriteLine(currentPlayer);
-             
-            // while (true)
-            // Console.WriteLine("Welcome to the Tic Tac Toe Game \n It is Player X goes first");
-            // DefaultBoard(instructionArray);
-            // Console.WriteLine("This is the current state with 1 marked as x");
-            // DefaultBoard(board);
-            // currentPlayer = ChangePlayer(currentPlayer);
-            // CurrentBoardState();
-            // Console.Write("Player {0}", currentPlayer);
-        
+            string gameState = "playOn";
+            string[,] currentBoard = defaultBoard;
+            while (gameState != "DONE")
+            {
+                if (gameState == "RESET")
+                {
+                    currentBoard = defaultBoard;
+                }
+                Console.WriteLine(instructions1);
+                PrintGameBoard(instructionBoard);
+                Console.WriteLine(instructions2);
+                PrintGameBoard(currentBoard);
+                //Console.WriteLine(gameState);
+                Console.WriteLine("Player {0}, it is your turn. Please select an empty square: ", currentPlayer );
+                string userSelection = Console.ReadLine();
+                currentBoard = PlayGame(userSelection, currentBoard, currentPlayer);
+                Console.WriteLine("Successful selection made"); // Pause here please for a few seconds                
+                gameState = CheckForWinningCombination(currentBoard, currentPlayer);
+                //Console.WriteLine(gameState);
+                currentPlayer = ChangePlayer(currentPlayer);
+                Console.Clear();
+            }            
         }
         
         static void PrintGameBoard(string [,] currentArray)
@@ -65,40 +54,8 @@ namespace Tic_Tac_Toe
                 Console.WriteLine("      |       |        ");
                 Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[2,0], squareArr[2,1], squareArr[2,2]);
                 Console.WriteLine("      |       |        \n"); 
-            // if (type == "instruction")
-            // {
-            //     Console.WriteLine("Here is the default board marking the spaces with the number representation");
-            //     string [,] squareArr = {{"0","1", "2"},{"3","4","5"},{"6","7", "8"}};                      
-            //     Console.WriteLine("\n      |       |        ");
-            //     Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[0,0], squareArr[0,1], squareArr[0,2]);
-            //     Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
-            //     Console.WriteLine("      |       |        ");
-            //     Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[1,0], squareArr[1,1], squareArr[1,2]);
-            //     Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
-            //     Console.WriteLine("      |       |        ");
-            //     Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[2,0], squareArr[2,1], squareArr[2,2]);
-            //     Console.WriteLine("      |       |        \n");
-                
-            // }
-            // else
-            // {
-            //     string [,] squareArr = currentArray;
-            //     Console.WriteLine("\n      |       |        ");
-            //     Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[0,0], squareArr[0,1], squareArr[0,2]);
-            //     Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
-            //     Console.WriteLine("      |       |        ");
-            //     Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[1,0], squareArr[1,1], squareArr[1,2]);
-            //     Console.WriteLine("_ _ _ | _ _ _ | _ _ _  ");
-            //     Console.WriteLine("      |       |        ");
-            //     Console.WriteLine("  {0}   |   {1}   |   {2}    ", squareArr[2,0], squareArr[2,1], squareArr[2,2]);
-            //     Console.WriteLine("      |       |        \n");                         
-            // }
         }     
 
-        static void CurrentBoardState()
-        {
-            Console.WriteLine("Hello, World");
-        }
         static string ChangePlayer(string currentPlayer)
         {
             if (currentPlayer == "X")
@@ -108,34 +65,46 @@ namespace Tic_Tac_Toe
             return "X";
         }
 
-        static string CheckForWinningCombination( string[,] currentArray)
+        static string CheckForWinningCombination( string[,] currentArray, string currentPlayer)
         {
-            string winner = "Player X";
-            return winner;
+            // Winning rows are 012, 345, 678 column are 036, 147, 258, horizontal 048, 246 but not if they are just a space
+            if (currentArray[0,0]==currentArray[0,1] && currentArray[0,2] == currentArray[0,1] && currentArray[0,0] != " "||
+            currentArray[1,0]==currentArray[1,1] && currentArray[1,2] == currentArray[1,0] && currentArray[1,0] != " " ||
+            currentArray[2,0]==currentArray[2,1] && currentArray[2,2] == currentArray[2,0] && currentArray[2,0] != " " ||
+            currentArray[0,0]==currentArray[1,0] && currentArray[2,0] == currentArray[0,0] && currentArray[0,0] != " " ||
+            currentArray[0,1]==currentArray[1,1] && currentArray[2,1] == currentArray[0,1] && currentArray[0,1] != " " ||
+            currentArray[0,2]==currentArray[1,2] && currentArray[2,2] == currentArray[0,2] && currentArray[0,2] != " " ||
+            currentArray[0,0]==currentArray[1,1] && currentArray[2,2] == currentArray[0,0] && currentArray[0,0] != " " ||
+            currentArray[0,2]==currentArray[1,1] && currentArray[2,0] == currentArray[2,2] && currentArray[0,2] != " ")
+            {
+                Console.WriteLine("Congratulations Player {0}, you are the winner!!!!\n \n Would you like to play again? (Y/N) ...", currentPlayer);
+                string playAgain = (Console.ReadLine()).ToUpper();
+                if (playAgain == "Y")
+                {
+                    currentArray = new string[,]{{" "," ", " "},{" "," "," "},{" "," ", " "}};
+                    return "RESET";
+                }
+                else if (playAgain == "N")
+                {
+                    return "DONE";
+                }
+                else
+                {
+                    Console.WriteLine("That was an invalid selection. Exiting now.");
+                    return "DONE";
+                }
+            }
+            return "playOn";
         }
-        static int CheckValidPlay (string userSelection, string[,] currentArray)
+        static string[,] PlayGame (string userSelection, string[,] currentArray, string currentPlayer)
+        // Method to check the users selection for validity - valid unoccupied square and square number
         {
-            // //https://www.dotnetperls.com/dictionary
-            // Dictionary<int, string> squares = new Dictionary<int, string>();
-            // // Temporary Variable for creating dictionary
-            // int squareNum = 0;
-            // for (int i = 0; i < 3; i++)
-            // {
-            //     for (int j = 0; j < 3; j++)
-            //     {                    
-            //         squares.Add(squareNum, currentArray[i,j]);
-            //         squareNum++;
-            //     }
-            // }
-            // foreach (KeyValuePair<int, string> key in squares)
-            // {
-            //     Console.WriteLine(key.Key + key.Value);
-            // }
             // Start of error checking. Adapated from Class.
             int userSquareSelection = 10;
             while (true)
             {
                 try
+                // First check to see if it is an integer
                 {
                     userSquareSelection = Convert.ToInt32(userSelection);                    
                 }    
@@ -143,13 +112,14 @@ namespace Tic_Tac_Toe
                 {
                     Console.WriteLine("That was not a number.");
                 }
-
+                // Check the state of the gameboard array to see if square is occupied, if not place mark in the square
                 switch (userSquareSelection)
                 {
                     case 0:
                         if (currentArray[0,0] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[0,0] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -158,7 +128,8 @@ namespace Tic_Tac_Toe
                     case 1:
                         if (currentArray[0,1] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[0,1] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -167,7 +138,8 @@ namespace Tic_Tac_Toe
                         case 2:
                         if (currentArray[0,2] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[0,2] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -176,7 +148,8 @@ namespace Tic_Tac_Toe
                         case 3:
                         if (currentArray[1,0] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[1,0] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -185,7 +158,8 @@ namespace Tic_Tac_Toe
                         case 4:
                         if (currentArray[1,1] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[1,1] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -194,7 +168,8 @@ namespace Tic_Tac_Toe
                         case 5:
                         if (currentArray[1,2] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[1,2] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -203,7 +178,8 @@ namespace Tic_Tac_Toe
                         case 6:
                         if (currentArray[2,0] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[2,0] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -212,7 +188,8 @@ namespace Tic_Tac_Toe
                         case 7:
                         if (currentArray[2,1] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[2,1] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
@@ -221,20 +198,14 @@ namespace Tic_Tac_Toe
                         case 8:
                         if (currentArray[2,2] == " ")
                         {
-                            return userSquareSelection;
+                            currentArray[2,2] = currentPlayer;
+                            return currentArray;
                         }
                         else
                         {
                             break;
                         }                        
-                }
-                // if (squares.TryGetValue(userSquareSelection, out squareResult))
-                // {
-                //     if (squareResult == " ")
-                //     {
-                //         return userSquareSelection;
-                //     }
-                // }    
+                } 
                 Console.WriteLine("That is an invalid square selection please try again!\nPlease try again by entering a valid square number:...");
                 userSelection = Console.ReadLine();           
             }
